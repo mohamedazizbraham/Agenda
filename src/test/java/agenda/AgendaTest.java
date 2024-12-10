be30e17df3c251f238881e79834d74363e261303
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-10 14:57:56 +0100

    initial commit

--- src/test/java/agenda/AgendaTest.java
+++ src/test/java/agenda/AgendaTest.java
@@ -1,19 +1,17 @@
 package agenda;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 public class AgendaTest {
     Agenda agenda;
-    
+
     // November 1st, 2020
     LocalDate nov_1_2020 = LocalDate.of(2020, 11, 1);
 
@@ -21,54 +19,58 @@ public class AgendaTest {
     LocalDate jan_5_2021 = LocalDate.of(2021, 1, 5);
 
     // November 1st, 2020, 22:30
-    LocalDateTime nov_1_2020_22_30 = LocalDateTime.of(2020, 11, 1, 22, 30);
+    LocalDateTime nov_1__2020_22_30 = LocalDateTime.of(2020, 11, 1, 22, 30);
 
     // 120 minutes
     Duration min_120 = Duration.ofMinutes(120);
 
-    // Un événement simple
+    // A simple event
     // November 1st, 2020, 22:30, 120 minutes
-    Event simple;
+    Event simple = new Event("Simple event", nov_1__2020_22_30, min_120);
 
-    // Un événement qui se répète toutes les semaines et se termine à une date donnée
-    Event fixedTermination;
+    // A Weekly Repetitive event ending at a given date
+    Repetition fixedTermination = new Termination("Fixed termination weekly", nov_1__2020_22_30, min_120, ChronoUnit.WEEKS, jan_5_2021);
 
-    // Un événement qui se répète toutes les semaines et se termine après un nombre donné d'occurrences
-    Event fixedRepetitions;
-    
+    // A Weekly Repetitive event ending after a give number of occurrrences
+    Repetition fixedRepetitions = new Termination("Fixed termination weekly", nov_1__2020_22_30, min_120, ChronoUnit.WEEKS, 10);
+
     // A daily repetitive event, never ending
-    // Un événement répétitif quotidien, sans fin
     // November 1st, 2020, 22:30, 120 minutes
-    Event neverEnding;
+    Repetition neverEnding = new Repetition("Never Ending", nov_1__2020_22_30, min_120, ChronoUnit.DAYS);
+
+    // Events to test the false issue in the test testIsFreeFor
+    Event simpleStartIn = new Event("Simple event", nov_1__2020_22_30.plusMinutes(10), min_120);
+    Event simpleFinishIn = new Event("Simple event", nov_1__2020_22_30.minusMinutes(10), min_120);
+    Event simpleIncludeIt = new Event("Simple event", nov_1__2020_22_30.minusMinutes(10), min_120.plusMinutes(20));
+
+    // Event to test the true issue in the test testIsFreeFor
+    Event otherSimple = new Event("Simple event", nov_1__2020_22_30.plusMinutes(130), min_120);
 
     @BeforeEach
     public void setUp() {
-        simple = new Event("Simple event", nov_1_2020_22_30, min_120);
-
-        fixedTermination = new Event("Fixed termination weekly", nov_1_2020_22_30, min_120);
-        fixedTermination.setRepetition(ChronoUnit.WEEKS);
-        fixedTermination.setTermination(jan_5_2021);
-
-        fixedRepetitions = new Event("Fixed termination weekly", nov_1_2020_22_30, min_120);
-        fixedRepetitions.setRepetition(ChronoUnit.WEEKS);
-        fixedRepetitions.setTermination(10);
-
-        neverEnding = new Event("Never Ending", nov_1_2020_22_30, min_120);
-        neverEnding.setRepetition(ChronoUnit.DAYS);
-
         agenda = new Agenda();
         agenda.addEvent(simple);
         agenda.addEvent(fixedTermination);
         agenda.addEvent(fixedRepetitions);
         agenda.addEvent(neverEnding);
     }
-    
+
     @Test
     public void testMultipleEventsInDay() {
-        assertEquals(4, agenda.eventsInDay(nov_1_2020).size(),
-            "Il y a 4 événements ce jour là");
+        assertEquals(4, agenda.eventsInDay(nov_1_2020).size(), "Il y a 4 événements ce jour là");
         assertTrue(agenda.eventsInDay(nov_1_2020).contains(neverEnding));
     }
 
+    @Test
+    public void testFindByTitle() {
+        assertEquals(1, agenda.findByTitle("Simple event").size(), "il y a un seul événement: Simple event");
+    }
 
+    @Test
+    public void testIsFreeFor() {
+        assertFalse(agenda.isFreeFor(simpleStartIn), "un événement ne peux pas commencer en meme temps qu'un autre");
+        assertFalse(agenda.isFreeFor(simpleFinishIn), "un événement ne peux pas terminer en meme temps qu'un autre");
+        assertFalse(agenda.isFreeFor(simpleIncludeIt), "un événement ne peux pas en contenir un autre");
+        assertTrue(agenda.isFreeFor(otherSimple), "ce creneau est sense etre libre");
+    }
 }
