a17f976c4685525149beb02c70581aff78f6a3cc
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-10 15:03:14 +0100

    Ajout de getNumberOfOccurrences

--- src/main/java/agenda/Termination.java
+++ src/main/java/agenda/Termination.java
@@ -1,25 +1,24 @@
 package agenda;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
 
-public class Termination {
-
-    public LocalDate terminationDateInclusive() {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
-    }
-
-    public long numberOfOccurrences() {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
-    }
-
+/**
+ * Description : A repetitive event that terminates after a given date, or after
+ * a given number of occurrences
+ */
+public class Termination extends Repetition {
+    private LocalDate terminationInclusive;
+    private Long numberOfOccurrences;
 
     /**
-     * Constructs a fixed termination event ending at a given date
+     * Constructs a fixed terminationInclusive event ending at a given date
+     *
+     * @param title the title of this event
      * @param start the start time of this event
+     * @param duration the duration of this event
      * @param frequency one of :
      * <UL>
      * <LI>ChronoUnit.DAYS for daily repetitions</LI>
@@ -27,16 +26,18 @@ public class Termination {
      * <LI>ChronoUnit.MONTHS for monthly repetitions</LI>
      * </UL>
      * @param terminationInclusive the date when this event ends
-     * @see ChronoUnit#between(Temporal, Temporal)
      */
-    public Termination(LocalDate start, ChronoUnit frequency, LocalDate terminationInclusive) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    public Termination(String title, LocalDateTime start, Duration duration, ChronoUnit frequency, LocalDate terminationInclusive) {
+        super(title, start, duration, frequency);
+        this.terminationInclusive = terminationInclusive;
     }
 
     /**
      * Constructs a fixed termination event ending after a number of iterations
+     *
+     * @param title the title of this event
      * @param start the start time of this event
+     * @param duration the duration of this event
      * @param frequency one of :
      * <UL>
      * <LI>ChronoUnit.DAYS for daily repetitions</LI>
@@ -45,9 +46,47 @@ public class Termination {
      * </UL>
      * @param numberOfOccurrences the number of occurrences of this repetitive event
      */
-    public Termination(LocalDate start, ChronoUnit frequency, long numberOfOccurrences) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    public Termination(String title, LocalDateTime start, Duration duration, ChronoUnit frequency, long numberOfOccurrences) {
+        super(title, start, duration, frequency);
+        this.numberOfOccurrences = numberOfOccurrences;
     }
 
+    /**
+     *
+     * @return the termination date of this repetitive event
+     */
+    public LocalDate getTerminationDate() {
+        if(terminationInclusive == null) {
+            switch (getFrequency()){
+                case DAYS :
+                    this.terminationInclusive = getStart().plusDays(this.numberOfOccurrences).toLocalDate();
+                case WEEKS:
+                    this.terminationInclusive = getStart().plusWeeks(this.numberOfOccurrences).toLocalDate();
+                case MONTHS:
+                    this.terminationInclusive = getStart().plusMonths(this.numberOfOccurrences).toLocalDate();
+            }
+        }
+        return terminationInclusive;
+    }
+
+    public long getNumberOfOccurrences() {
+        long nbJours = (getTerminationDate().getDayOfYear() + getTerminationDate().getYear()*365) - (getStart().getDayOfYear() + getStart().getYear()*365);
+        if(numberOfOccurrences == null) {
+            switch (getFrequency()){
+                case DAYS :
+                    numberOfOccurrences = nbJours;
+                case WEEKS:
+                    numberOfOccurrences = nbJours/7;
+                case MONTHS:
+                    numberOfOccurrences = nbJours/30;
+            }
+        }
+        return numberOfOccurrences;
+    }
+
+    @Override
+    public boolean isInDay(LocalDate aDay) {
+        if (aDay.isAfter(getTerminationDate())) return false;
+        return super.isInDay(aDay);
+    }
 }
